9ca988816ae0f42152dbab9eace08445abef9531
Claudio Rodriguez <claudio.rdgz@gmail.com>
2018-06-12 23:35:05 -0400

    feat: finish the thing

--- src/App.js
+++ src/App.js
@@ -1,31 +1,71 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 import GraphiQL from 'graphiql'
 import fetch from 'isomorphic-fetch'
 import 'graphiql/graphiql.css'
 
+function getFetcher(url, apiKey) {
+  function graphQLFetcher(graphQLParams) {
+    return fetch(url, {
+      method: 'post',
+      headers: { 
+        'x-api-key': apiKey,
+        'Content-Type': 'application/json' 
+      },
+      body: JSON.stringify(graphQLParams),
+    }).then(response => response.json())
+  }
+  return graphQLFetcher
+}
 
-function graphQLFetcher(graphQLParams) {
-  return fetch(undefined, {
-    method: 'post',
-    headers: { 
-      'Content-Type': 'application/json' 
-    },
-    body: JSON.stringify(graphQLParams),
-  }).then(response => response.json())
+const GraphiQLEnv = (props) => {
+  if (!props.url || !props.apiKey) {
+    return <div class="warn-msg">No URL and API Key set</div>
+  }
+  return <GraphiQL fetcher={getFetcher(props.url, props.apiKey)} />
 }
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.url = ''
+    this.apiKey = ''
+    this.state = {
+      url: '',
+      apiKey: ''
+    }
+  }
+  
+  handleUrl (ev) {
+    this.url = ev.target.value
+  }
+
+  handleApiKey (ev) {
+    this.apiKey = ev.target.value
+  }
+
+  submitButton () {
+    this.setState({
+      url: this.url,
+      apiKey: this.apiKey
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <input type="text" className="App-url" name="url" />
-          <input type="text" className="App-api-key" name="apiKey" />
-          <button type="submit" className="App-apply">Apply</button>
+          <div class="header-item">
+            <label className="App-url-label" for="url">GraphQL URL</label>
+            <input type="text" className="App-url" name="url" onChange={this.handleUrl.bind(this)} />
+          </div>
+          <div class="header-item">
+            <label className="App-api-key-label" for="apiKey">API KEY</label>
+            <input type="text" className="App-api-key" name="apiKey" onChange={this.handleApiKey.bind(this)} />
+          </div>
+          <button type="submit" className="App-apply" onClick={this.submitButton.bind(this)}>Apply</button>
         </header>
-        <GraphiQL fetcher={graphQLFetcher} />
+        {GraphiQLEnv(this.state)}
       </div>
     );
   }
